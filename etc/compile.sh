4acd76023f463f382550cda180b8850f8565345a
Bob Weigel <rweigel@gmu.edu>
2026-09-20 22:26:05 -0400

    remove trackchanges

--- etc/compile.sh
+++ etc/compile.sh
@@ -9,19 +9,19 @@ cp submitted2-*.pdf ../../
 
 cd ../..
 
-$PDFLATEX submitted2-track.tex
-$BIBTEX submitted2-track
-$PDFLATEX submitted2-track.tex
-$PDFLATEX submitted2-track.tex
+#$PDFLATEX submitted2-track.tex
+#$BIBTEX submitted2-track
+#$PDFLATEX submitted2-track.tex
+#$PDFLATEX submitted2-track.tex
 
-sed 's/\\mbox{\\cite\([^}]*\)}/\\cite\1/g' submitted2-track.tex > submitted2.tex
+#sed 's/\\mbox{\\cite\([^}]*\)}/\\cite\1/g' submitted2-track.tex > submitted2.tex
 #cp submitted2-track.tex submitted2.tex
-sed -i '' 's/\\usepackage\[inline\]/\\usepackage[finalnew]/' submitted2.tex
+#sed -i '' 's/\\usepackage\[inline\]/\\usepackage[finalnew]/' submitted2.tex
 
-$PDFLATEX submitted2.tex
-$BIBTEX submitted2
-$PDFLATEX submitted2.tex
-$PDFLATEX submitted2.tex
+$PDFLATEX submitted3.tex
+$BIBTEX submitted3
+$PDFLATEX submitted3.tex
+$PDFLATEX submitted3.tex
 
 
 
